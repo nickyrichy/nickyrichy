ced2efa5f02372115324ed4d5708fd1632326555
ryanli <707417775@qq.com>
2022-10-20 23:34:26 +0800

    Update core.js

--- assets/js/core.js
+++ assets/js/core.js
@@ -7,20 +7,22 @@ $(document).ready(function () {
     var user = webURL.split(splitFlag)[1].split(".")[0];
     //user = 'nickyrichy';
     blogListURL = 'https://api.github.com/repos/' + user + '/' + user + '.github.io/contents/notes';
+	
 
     //set Blog list
     $.getJSON(blogListURL, function (json) {
 
         var name = json[0].name; // Blog title
-
-
+        
+		
         var blogURL = json[0].download_url; //Blog Raw Url
 
         var type = "markdown";
-        if (name.substr(-5, 5) == ".html") {
+		if (name.substr(-5, 5) == ".html") {
             name = name.substr(0, name.length - 5);
             type = "html";
         }
+
 
         // set blog content
         $.get(blogURL, function (result) {
@@ -35,7 +37,7 @@ $(document).ready(function () {
                     htmlDecode: "style,script,iframe", // you can filter tags decode
                     //toc             : false,
                     tocm: true, // Using [TOCM]
-                    tocContainer: "#custom-toc-container", // 自定义 ToC 容器层
+                    tocContainer    : "#custom-toc-container", // 自定义 ToC 容器层
                     //gfm             : false,
                     //tocDropdown     : true,
                     // markdownSourceCode : true, // 是否保留 Markdown 源码，即是否删除保存源码的 Textarea 标签
@@ -44,36 +46,10 @@ $(document).ready(function () {
                     tex: true, // 默认不解析
                     flowChart: true, // 默认不解析
                     sequenceDiagram: true, // 默认不解析
-
-                    onscroll: function (event) {
-                        console.clear();
-                        console.log("onscroll =>", this, this.id, this.settings, event);
-                    },
-
-                    onpreviewscroll: function (event) {
-                        console.clear();
-                        console.log("onpreviewscroll =>", this, this.id, this.settings, event);
-                    },
-
-                    onload: function () {
-                        this.off("previewscroll"); // unbind before handle
-
-                        // Override settings.onpreviewscroll
-                        this.on("previewscroll", function () {
-                            console.clear();
-                            console.log("on() => Override settings.onpreviewscroll =>", this, this.id, event, (new Date).getTime());
-                        });
-
-                        // defined event bind
-                        this.on("resize", function () {
-                            console.clear();
-                            console.log("onresize =>", this, this.id, event, (new Date).getTime());
-                        });
-                    }
                 });
 
             } else {
-
+               
                 $("#article").html(result);
             }
 
